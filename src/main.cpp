ec2f77019daa97cc401f61a2eb87d768c655de91
Vankeep <mashkovvan93@yandex.ru>
2025-09-01 19:21:37 +0300

    mutex

--- src/main.cpp
+++ src/main.cpp
@@ -3,33 +3,56 @@
 
 #define LED PC13
 
-// Функция задачи для мигания LED
-void blinkTask(void *pvParameters) {
-  for (;;) {
-    digitalWrite(LED, HIGH);  // Включить LED
-    vTaskDelay(1000 / portTICK_PERIOD_MS);  // Задержка 1 секунда
-    digitalWrite(LED, LOW);   // Выключить LED
-    vTaskDelay(1000 / portTICK_PERIOD_MS);  // Задержка 1 секунда
+HardwareSerial uartPort(PA10, PA9);
+
+SemaphoreHandle_t xMutex;
+
+// Задача 1: отправка каждые 300 msec
+void xTask1(void *pvParam){
+  while (1){
+    // Захватываем мьютекс xSemaphoreTake. portMAX_DELAY (ждем бесконечно)
+    // portMAX_DELAY это - Я готов ждать столько, сколько потребуется. Не возвращай управление моей задаче, пока я не получу то, что жду. Тем временем ты можешь выполнять другие задачи.
+    // Используя portMAX_DELAY, вы должны быть уверены, что рано или поздно событие произойдет 
+    //    (мьютекс освободится, данные поступят в очередь). 
+    //    Иначе задача заблокируется навсегда, что может привести к "зависанию" всей системы.
+    if(xSemaphoreTake(xMutex, portMAX_DELAY) == pdTRUE){
+      uartPort.println("Task 1: Hello from Task 1!");
+      xSemaphoreGive(xMutex); // Освобождаем мьютекс
+    }
+    vTaskDelay(pdMS_TO_TICKS(300)); // Ждем 300 мсек
   }
 }
 
-void setup() {
-  pinMode(LED, OUTPUT);  // Настроить пин LED как выход
-  
-  // Создать задачу FreeRTOS
-  xTaskCreate(
-    blinkTask,          // Функция задачи
-    "Blink",            // Имя задачи (для отладки)
-    128,                // Размер стека (в словах)
-    NULL,               // Параметры (не используются)
-    1,                  // Приоритет (1 - нормальный)
-    NULL                // Хэндл задачи (не используется)
-  );
-  
-  // Запустить планировщик FreeRTOS
-  vTaskStartScheduler();
+// Задача 2: отправка каждые 700 msec
+void xTask2(void *pvParam){
+  while (1){
+    if(xSemaphoreTake(xMutex, portMAX_DELAY) == pdTRUE){
+      uartPort.println("Task 2: Message from Task 2!");
+      xSemaphoreGive(xMutex);
+    }
+    vTaskDelay(pdMS_TO_TICKS(700)); // Ждем 700 мсек
+  }
 }
 
-void loop() {
-  // Пустой цикл: все действия в задачах FreeRTOS
+void setup(){
+  uartPort.begin(1200);
+
+  xMutex = xSemaphoreCreateMutex();
+  
+  if(xMutex != NULL){
+    xTaskCreate(xTask1, "t1", 128, NULL, 1, NULL);
+    xTaskCreate(xTask2, "t2", 128, NULL, 1, NULL);
+
+    vTaskStartScheduler();
+  } 
+
+  // Если планировщик не запустился, мигаем светодиодом быстро
+  pinMode(PC13, OUTPUT);
+  while(1) {
+    digitalToggle(PC13);
+    delay(100);
+  }
+  
 }
+
+void loop() {}
